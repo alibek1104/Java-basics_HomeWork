e6e301115aee385ab56ba8734982aa2106e20d9b
Alibek <nurmahanovalibek@gmail.com>
2023-05-04 19:47:12 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Привет, мир! Я изучаю Java");
+        //System.out.println("Hello, world! I am learning Java"); //правда, то что я только начал изучать (это однострочный комментарий)
+        /* правда, то что я только начал изучать
+        (это многострочный комментарий)
+         */
+    }
+}
